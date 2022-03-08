1b51304925890ba023aa5bc0468748c08a8e2312
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-03-08 21:08:29 +0530

    Update testReadingsFromRange.cpp

--- testReadingsFromRange.cpp
+++ testReadingsFromRange.cpp
@@ -7,8 +7,7 @@
 
 TEST_CASE("Test readings from ranges")
 {
-  vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};
-  int lowerBound=3;
-  int upperBound=5;   
-  REQUIRE(getReadingsForTheRange(chargingSessionSamples, lowerBound, upperBound) == 4);  
+  vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
+  REQUIRE(getReadingsForTheRange(chargingSessionSamples, 3, 5) == 4);
+  REQUIRE(getReadingsForTheRange(chargingSessionSamples, 10, 12) == 3);
 }
