e4cf218cac89932a15ded40742ce920711ab13bf
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-03-08 20:31:39 +0530

    Update ReadingsFromRange.cpp

--- ReadingsFromRange.cpp
+++ ReadingsFromRange.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int getReadingsForTheRange(const int *chargingSessionSamples, int lowerBound, int upperBound) {
-  int sizeofchargingSessionSamples = sizeof(chargingSessionSamples) / sizeof(chargingSessionSamples[0]);
+  int sizeofchargingSessionSamples = sizeof((chargingSessionSamples) / sizeof(chargingSessionSamples[0]));
   int countOfReadingsFromRange = 0;
   for(int i = 0; i < sizeofchargingSessionSamples; i++) {
     if((chargingSessionSamples[i] >= lowerBound) && (chargingSessionSamples[i] <= upperBound)) {
