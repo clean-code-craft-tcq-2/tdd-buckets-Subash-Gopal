ab19b0714c89919fbe69bcb0f01db558651b1710
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-03-08 20:10:59 +0530

    Update ReadingsFromRange.h

--- ReadingsFromRange.h
+++ ReadingsFromRange.h
@@ -1,4 +1,4 @@
 #include <iostream>
 #include<string>
 using namespace std;
-int getReadingsFromRanges(const int *chargingSessionSamples, int lowerBound, int upperBound);
+int getReadingsForTheRange(const int *chargingSessionSamples, int lowerBound, int upperBound);
