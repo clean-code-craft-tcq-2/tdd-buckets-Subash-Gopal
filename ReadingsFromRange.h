28ecac47b90455615c55b4e6869938c565d04e02
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-03-09 09:57:58 +0530

    Update ReadingsFromRange.h

--- ReadingsFromRange.h
+++ ReadingsFromRange.h
@@ -2,4 +2,6 @@
 #include<string>
 #include <vector>
 using namespace std;
+bool isPreConditionsMet(int sizeofChargingSessionSamples);
 int getReadingsForTheRange(vector<int> chargingSessionSamples, int lowerBound, int upperBound);
+int countReadingsForRange(vector<int> chargingSessionSamples, int lowerBound, int upperBound)
