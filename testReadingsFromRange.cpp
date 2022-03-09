049377489645b5c41585317ea757b78ed2d5dd7e
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-03-09 10:53:04 +0530

    Update testReadingsFromRange.cpp

--- testReadingsFromRange.cpp
+++ testReadingsFromRange.cpp
@@ -37,6 +37,7 @@ TEST_CASE("Test readings count with invalid ranges values")
 {
   vector <int> chargingSessionSamples = {3, 3, 5, 4, 10, 11, 12};   
   REQUIRE(getReadingsForTheRange(chargingSessionSamples, -1, -5) ==  0);
+   REQUIRE(getReadingsForTheRange(chargingSessionSamples, -5, -1) ==  0);
   REQUIRE(getReadingsForTheRange(chargingSessionSamples, 14, 100) == 0);
 }
 
