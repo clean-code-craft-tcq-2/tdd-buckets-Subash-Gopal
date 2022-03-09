7c732951fc1b116f0880fde9759a05f3bb2d4911
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-03-09 09:58:26 +0530

    Update ReadingsFromRange.cpp

--- ReadingsFromRange.cpp
+++ ReadingsFromRange.cpp
@@ -6,17 +6,26 @@
 
 using namespace std;
 
+bool isPreConditionsMet(int sizeofChargingSessionSamples) {
+  return (sizeofChargingSessionSamples > 1)? true : false;
+}
+  
+
 int getReadingsForTheRange(vector<int> chargingSessionSamples, int lowerBound, int upperBound) {
-  int sizeofchargingSessionSamples = chargingSessionSamples.size();
-  if(sizeofchargingSessionSamples > 0)
-  {
+  int sizeofChargingSessionSamples = chargingSessionSamples.size();
+  if(isPreConditionsMet(sizeofChargingSessionSamples) == true) {
+    return countReadingsForRange(chargingSessionSamples,lowerBound,upperBound);
+  }  
+  return -1;  
+}
+
+int countReadingsForRange(vector<int> chargingSessionSamples, int lowerBound, int upperBound) {
+{
     int countOfReadingsFromRange = 0;
-    for(int i = 0; i < sizeofchargingSessionSamples; i++) {
+    for(int i = 0; i <  chargingSessionSamples.size(); i++) {
       if((chargingSessionSamples[i] >= lowerBound) && (chargingSessionSamples[i] <= upperBound)) {
         countOfReadingsFromRange++;
       }
     } 
     return countOfReadingsFromRange;
-  }
-  return -1;  
 }
